a6a2dfdd2db410fca9dbafc553c7a2367f35f8a8
Christophe Phillips <c.phillips@uliege.be>
2025-07-25 14:17:25 +0200

    Add skeleton 'main' and 1st step function

---
+++ crc_qMRIage_01_prepdata.m
@@ -0,0 +1,10 @@
+function aa = crc_qMRIage_01_prepdata
+% 
+%_______________________________________________________________________
+% Copyright (C) 2025 Cyclotron Research Centre
+
+% Written by 
+% - C. Phillips, Cyclotron Research Centre, University of Liege, Belgium
+% - S. Moallemian, Rutgers University, NJ, USA
+
+end
---
+++ crc_qMRIage_main.m
@@ -0,0 +1,13 @@
+function aa = crc_qMRIage_main
+% Main function for thre processing of the MPM qMRI aging data set.
+% This calls a series of functions to deal with the successive processing
+% steps, from data cleaning to figure production.
+%_______________________________________________________________________
+% Copyright (C) 2025 Cyclotron Research Centre
+
+% Written by 
+% - C. Phillips, Cyclotron Research Centre, University of Liege, Belgium
+% - S. Moallemian, Rutgers University, NJ, USA
+
+
+end
