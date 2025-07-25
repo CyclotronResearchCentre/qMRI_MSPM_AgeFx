5b40d043625da68992731f80dd4974df820f9b2d
Christophe Phillips <c.phillips@uliege.be>
2025-07-25 18:11:26 +0200

    Define empty Matlabbatch

---
+++ MBatch_1Sttest_empty.m
@@ -0,0 +1,36 @@
+%-----------------------------------------------------------------------
+% Job saved on 25-Jul-2025 17:35:39 by cfg_util (rev $Rev: 8183 $)
+% spm SPM - SPM25 (00.00)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.stats.factorial_design.dir = '<UNDEFINED>';
+matlabbatch{1}.spm.stats.factorial_design.des.t1.scans = '<UNDEFINED>';
+matlabbatch{1}.spm.stats.factorial_design.cov(1).c = '<UNDEFINED>';
+matlabbatch{1}.spm.stats.factorial_design.cov(1).cname = '<UNDEFINED>';
+matlabbatch{1}.spm.stats.factorial_design.cov(1).iCFI = 1;
+matlabbatch{1}.spm.stats.factorial_design.cov(1).iCC = 1;
+matlabbatch{1}.spm.stats.factorial_design.cov(2).c = '<UNDEFINED>';
+matlabbatch{1}.spm.stats.factorial_design.cov(2).cname = '<UNDEFINED>';
+matlabbatch{1}.spm.stats.factorial_design.cov(2).iCFI = 1;
+matlabbatch{1}.spm.stats.factorial_design.cov(2).iCC = 1;
+matlabbatch{1}.spm.stats.factorial_design.cov(3).c = '<UNDEFINED>';
+matlabbatch{1}.spm.stats.factorial_design.cov(3).cname = '<UNDEFINED>';
+matlabbatch{1}.spm.stats.factorial_design.cov(3).iCFI = 1;
+matlabbatch{1}.spm.stats.factorial_design.cov(3).iCC = 1;
+matlabbatch{1}.spm.stats.factorial_design.cov(4).c = '<UNDEFINED>';
+matlabbatch{1}.spm.stats.factorial_design.cov(4).cname = '<UNDEFINED>';
+matlabbatch{1}.spm.stats.factorial_design.cov(4).iCFI = 1;
+matlabbatch{1}.spm.stats.factorial_design.cov(4).iCC = 1;
+matlabbatch{1}.spm.stats.factorial_design.multi_cov = struct('files', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.masking.tm.tm_none = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.im = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.em = '<UNDEFINED>';
+matlabbatch{1}.spm.stats.factorial_design.globalc.g_omit = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.gmsca.gmsca_no = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.glonorm = 1;
+matlabbatch{2}.spm.stats.review.spmmat(1) = cfg_dep('Factorial design specification: SPM.mat File', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{2}.spm.stats.review.display.matrix = 1;
+matlabbatch{2}.spm.stats.review.print = 'pdf';
+matlabbatch{3}.spm.stats.fmri_est.spmmat(1) = cfg_dep('Factorial design specification: SPM.mat File', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{3}.spm.stats.fmri_est.write_residuals = 0;
+matlabbatch{3}.spm.stats.fmri_est.method.Classical = 1;
