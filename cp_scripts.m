4865ab919e305dda03907fc66869809cf17fd8e5
Christophe Phillips <c.phillips@uliege.be>
2026-03-20 00:02:56 +0100

    Further scripting

--- cp_scripts.m
+++ cp_scripts.m
@@ -48,7 +48,30 @@ NVx = Nvx_per_clust_CV1{1,1}
 sum(NVx{1,1}==1)
 
 %% Looking at Cohen's Kappa
+% Test
 fn_bin_test = char( fn_out{1,1}{1}, fn_out{1,5}{1})
 CK = crc_CohenKappaImg(fn_bin_test)
 % CK = crc_CohenKappaImg(fn_bin_test,fn_msk)
+
+% USeful tests
+CKall = crc_check_allCK
+
+%% Zipping of .nii files from some specific set of folder
+
+% Defaults & input
+[pth,fn] = crc_qMRIage_defaults;
+% filt_dir = '.*binarized.*';
+filt_dir = '.*SPM.*';
+l_dirs = spm_select('FPList',pth.deriv,'dir',filt_dir)
+
+% Zipping
+flag_gzip = struct(...
+    'filt','^.*\.nii$',... % all .nii files
+    'rec', true, ... % act recursively
+    'delOrig', true);      % delete original file after gzipping 
+for i_dir = 1:size(l_dirs,1)
+    fn_out = crc_gzip(deblank(l_dirs(i_dir,:)), flag_gzip);
+end
+
+
 
